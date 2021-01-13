f7510b075687b9b5240d5c2e31bdbbbdce8ffceb
Ferdinand Malcher <ferdinand@malcher.media>
2021-01-13 14:00:03 +0100

    BookComponent und Property Binding

---
+++ book-rating/src/app/book/book.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Book } from '../shared/book';
+
+@Component({
+  selector: 'br-book',
+  templateUrl: './book.component.html',
+  styleUrls: ['./book.component.scss']
+})
+export class BookComponent implements OnInit {
+
+  @Input() book: Book;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
