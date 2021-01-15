be95c2ca55a1e070b29751bd8e254b9508d813e4
Ferdinand Malcher <ferdinand@malcher.media>
2021-01-15 13:18:28 +0100

    Tests Dashboard und AppComponent

--- book-rating/src/app/books/dashboard/dashboard.component.spec.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.spec.ts
@@ -1,14 +1,35 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Book } from '../shared/book';
+import { BookRatingService } from '../shared/book-rating.service';
 
 import { DashboardComponent } from './dashboard.component';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
+  let ratingMock;
+  let book: Book;
 
   beforeEach(async () => {
+    book = {
+      isbn: '',
+      title: '',
+      description: '',
+      price: 4,
+      rating: 3
+    };
+
+    ratingMock = {
+      // rateUp: () => book
+      rateUp: b => b
+    };
+
     await TestBed.configureTestingModule({
-      declarations: [ DashboardComponent ]
+      declarations: [ DashboardComponent ],
+      providers: [
+        // Service ausmocken: Wenn BRS angefordert wird, dann ratingMock ausliefern
+        { provide: BookRatingService, useValue: ratingMock }
+      ]
     })
     .compileComponents();
   });
@@ -21,5 +42,14 @@ describe('DashboardComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call the service for doRateUp', () => {
+    const rs = TestBed.inject(BookRatingService);
+    spyOn(rs, 'rateUp').and.callThrough(); // Aufruf zum originalen "rs" durchleiten, nicht blockieren
+
+    component.doRateUp(book);
+
+    expect(rs.rateUp).toHaveBeenCalled();
   });
 });
