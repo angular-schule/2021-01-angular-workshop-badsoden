26e4212ff820a680bde3dd32f91beae9661fbafc
Ferdinand Malcher <ferdinand@malcher.media>
2021-01-15 11:20:52 +0100

    Tests BookComponent

--- book-rating/src/app/books/book/book.component.spec.ts
+++ book-rating/src/app/books/book/book.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Book } from '../shared/book';
 
 import { BookComponent } from './book.component';
 
@@ -16,10 +17,37 @@ describe('BookComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(BookComponent);
     component = fixture.componentInstance;
+
+    // Property belegen, denn es gibt ja keine Elternkomponente
+    component.book = {
+      isbn: '',
+      title: '',
+      description: '',
+      price: 3,
+      rating: 4
+    };
+
+    // Change Detection ausführen, NACHDEM book belegt ist
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should emit an event for doRateUp', () => {
+    // Arrange
+    let emittedBook: Book;
+
+    component.rateUp.subscribe(book => {
+      emittedBook = book;
+    });
+
+    // Act
+    component.doRateUp();
+
+    // Assert
+    expect(emittedBook).toBeDefined();
+    expect(emittedBook).toBe(component.book);
   });
 });
