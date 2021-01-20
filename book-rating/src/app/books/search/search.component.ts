1753fc0c89c1735fce1d5002afbed12b2221e1d1
Ferdinand Malcher <ferdinand@malcher.media>
2021-01-20 13:30:05 +0100

    Typeahead-Suche

--- book-rating/src/app/books/search/search.component.ts
+++ book-rating/src/app/books/search/search.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { debounceTime, filter, switchMap, tap } from 'rxjs/operators';
+import { Book } from '../shared/book';
+import { BookStoreService } from '../shared/book-store.service';
 
 @Component({
   selector: 'br-search',
@@ -10,12 +14,32 @@ export class SearchComponent implements OnInit {
 
   searchControl: FormControl;
 
-  constructor() { }
+  results$: Observable<Book[]>;
+  loading = false;
+
+  constructor(private bs: BookStoreService) { }
 
   ngOnInit(): void {
     this.searchControl = new FormControl('');
 
-    this.searchControl.valueChanges.subscribe(e => console.log(e));
+    this.results$ = this.searchControl.valueChanges.pipe(
+      filter((e: string) => e.length >= 3),
+      debounceTime(1000),
+      tap(() => this.loading = true),
+      switchMap(term => this.bs.search(term)),
+      tap(() => this.loading = false),
+    );
   }
 
+  /*
+  Anforderungen Typeahead:
+  - während der Eingabe suchen
+  - Suchbegriff mindestens 3 Zeichen
+  - erst abschicken, wenn Nutzer Finger still hält
+  - this.bs.search(term)
+  - Bücher anzeigen (ganz simpel!)
+  - Zusatz: AsyncPipe
+  - Zusatz: Ladeanzeige
+  - Zusatz: Meldung anzeigen, wenn keine Bücher vorhanden sind
+  */
 }
